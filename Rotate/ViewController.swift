f3d76b516e0a61dca6d8d81759702679a6b4ac6c
Quan Teng Foong <e0564970@u.nus.edu>
2023-06-20 22:37:58 +0800

    Initial Commit

---
+++ Rotate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rotate
+//
+//  Created by Quan Teng Foong on 20/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
